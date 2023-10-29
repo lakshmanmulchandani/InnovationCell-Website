ffb604eae4874507218c068341dd65a734623fd6
Alim Khan <90334104+Mh-Alim@users.noreply.github.com>
2023-10-29 16:15:09 +0530

    core coordinator team section added

--- client/src/Components/Team/Team.jsx
+++ client/src/Components/Team/Team.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Team.css";
 import TeamCard from "./TeamCard";
+import NameCard from "./NameCard";
 
 const Team = () => {
   return (
     <>
       <div id="team" className="teamHeading">
-        <h1>Our Team</h1>
+        <h1>Team Innovation cell</h1>
       </div>
       <div className="Teams">
         <TeamCard name="Alim Khan" domain ="Tech Domain"  />
@@ -18,7 +19,28 @@ const Team = () => {
         <TeamCard name="Alim Khan" domain ="Tech Domain"  />
         <TeamCard name="Alim Khan" domain ="Tech Domain"  />
         {/* <Chart /> */}
+
       </div>
+
+      <div className="teamHeading">
+        <h1>Core coordinators</h1>
+      </div>
+
+      <div className="Teams">
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+      <NameCard name={"Lakshmna Mulnchnahd"} domain={"Technical"} />
+
+        {/* <Chart /> */}
+
+      </div>
+      
     </>
   );
 };
