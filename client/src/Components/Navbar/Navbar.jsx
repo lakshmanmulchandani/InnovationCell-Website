421f3a18ae94a5de47659be16e6d830a98eca99c
Prince <princetripathi093@gmail.com>
2023-11-06 21:28:29 +0530

    updated

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,51 +1,49 @@
-import React from 'react'
-import './navbar.css'
-import logo from '../../Assets/images/i_cell_logo.png'
+import React from "react";
+import "./navbar.css";
+import logo from "../../Assets/images/i_cell_logo.png";
 
 const Navbar = () => {
   return (
-  
-      <div className='nav'>
-        <div className='logo'>
-          <a href='/'>
+    <div className="nav">
+      <div className="logo">
+        <a href="/">
           <img src={logo} alt="I-Cell Logo" />
+        </a>
+      </div>
+      <div className="nav-opt">
+        <div className="nav-btn">
+          <a href="#">
+            <div>Home</div>
           </a>
         </div>
-        <div className='nav-opt'>
-          <div className='nav-btn'>
-            <a href='#'>
-              <div >Home</div>
-            </a>
-          </div>
-          <div className='nav-btn'>
-            <a href='#'>
-              <div >Events</div>
-            </a>
-          </div>
-          <div className='nav-btn'>
-            <a href='#'>
-              <div >Team</div>
-            </a>
-          </div>
-          <div className='nav-btn'>
-            <a href='#'>
-              <div >Sponsors</div>
-            </a>
-          </div>
-          <div className='nav-btn'>
-            <a href='#'>
-              <div >AboutUs</div>
-            </a>
-          </div>
-          <div className='nav-btn'>
-            <a href='#'>
-              <div >ContactUs</div>
-            </a>
-          </div>
+        <div className="nav-btn">
+          <a href="#">
+            <div>Events</div>
+          </a>
+        </div>
+        <div className="nav-btn">
+          <a href="#">
+            <div>Team</div>
+          </a>
+        </div>
+        <div className="nav-btn">
+          <a href="#">
+            <div>Sponsors</div>
+          </a>
+        </div>
+        <div className="nav-btn">
+          <a href="#">
+            <div>AboutUs</div>
+          </a>
+        </div>
+        <div className="nav-btn">
+          <a href="#">
+            <div>ContactUs</div>
+          </a>
         </div>
       </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
