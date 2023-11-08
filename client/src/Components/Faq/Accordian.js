b5d28bd9e60d6b3c39cdf778884978cbd0af961d
Azhar Ansari <azharansari@Azhars-MacBook-Air.local>
2023-11-08 20:20:12 +0530

    added FAQ section

---
+++ client/src/Components/Faq/Accordian.js
@@ -0,0 +1,72 @@
+import React, { useRef, useState } from "react";
+import data from "./AccordianData";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import MAN2 from "../../imgs/MAN2.png";
+import "./Accordian.css";
+
+//  accordionitem component
+const AccordionItem = ({ question, answer, isOpen, onClick }) => {
+  const contentHeight = useRef();
+  return (
+    <div className="wrapper">
+      <button
+        className={`question-container ${isOpen ? "active" : ""}`}
+        onClick={onClick}
+      >
+        <p className="question-content">{question}</p>
+        <RiArrowDropDownLine className={`arrow ${isOpen ? "active" : ""}`} />
+      </button>
+
+      <div
+        ref={contentHeight}
+        className="answer-container"
+        style={
+          isOpen
+            ? { height: contentHeight.current.scrollHeight }
+            : { height: "0px" }
+        }
+      >
+        <p className="answer-content">{answer}</p>
+      </div>
+    </div>
+  );
+};
+
+const Accordion = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleItemClick = (index) => {
+    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
+  return (
+    <div>
+            <div className="teamHeading">
+        <h1>FAQ</h1>
+      </div>
+
+      <div className="acc-container" style={{ position: "relative" }}>
+        <div style={{ width: "40%" }}>
+          <img
+            className="snowman"
+            style={{  top: "50px", height:"650px" }}
+            src={MAN2}
+          />
+        </div>
+        <div className="container" style={{ width: "60%" }}>
+          {data.map((item, index) => (
+            <AccordionItem
+              key={index}
+              question={item.question}
+              answer={item.answer}
+              isOpen={activeIndex === index}
+              onClick={() => handleItemClick(index)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
