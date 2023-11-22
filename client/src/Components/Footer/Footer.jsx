3135e476ba012cfd60a95333e4e5fa36db1b8115
Rohit <rohitmondall8000@gmail.com>
2023-11-23 00:21:24 +0530

    Footer section

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -26,51 +26,60 @@ const footerLinks = [
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="footer-logo">
-        <img src={ICellImg} alt="Icell Logo" />
-        <h2>Innovation Cell</h2>
-      </div>
-
-
-      <div className="about-container container">
-        <h1>I-Cell</h1>
-
-        <div className="about">
-          <a href="#aboutusnav">About Us</a>
-          <a href="#eventsnav">Events</a>
-          <a href="#spons">Sponsors</a>
+    <footer className="footer">
+      <div className="upper-section">
+        <div className="footer-logo">
+          <img src={ICellImg} alt="Icell Logo" />
+          <h2>Innovation Cell</h2>
         </div>
-      </div>
 
 
-      <div className="container">
-        <h1>Contact</h1>
+        <div className="about-container container">
+          <h1>I-Cell</h1>
 
-        <div className="contacts">
-          <a href="https://maps.app.goo.gl/EWSsuCfKTZvQZBxCA" target="_blank" rel="noreferrer">
-            <BsPinMapFill style={{ margin: "1px 15px 0 0", scale: "150%" }} />
-            <span>NIT Raipur, Raipur, CG 492013</span>
-          </a>
-
-          <a href="mailto:info@icell.io" target="_blank" rel="noreferrer">
-            <BsEnvelope style={{ margin: "1px 15px 0 0", scale: "150%" }} />
-            <span>Email</span>
-          </a>
+          <div className="about">
+            <a href="#aboutusnav">About Us</a>
+            <a href="#eventsnav">Events</a>
+            <a href="#spons">Sponsors</a>
+            <a href="#team">Team</a>
+          </div>
         </div>
-      </div>
 
 
-      <div className="social-container">
-        <h1>Let&apos;s Connect</h1>
+        <div className="container">
+          <h1>Contact</h1>
 
-        <div className="socials">
-          {footerLinks.map((links, idx) => (
-            <a key={idx} href={links.link} target="_blank">
-              <links.icon />
+          <div className="contacts">
+            <a href="https://maps.app.goo.gl/EWSsuCfKTZvQZBxCA" target="_blank" rel="noreferrer">
+              <BsPinMapFill style={{ scale: "120%" }} />
+              <span>NIT Raipur, Raipur, CG 492013</span>
             </a>
-          ))}
+
+            <a href="mailto:info@icell.io" target="_blank" rel="noreferrer">
+              <BsEnvelope style={{ scale: "120%" }} />
+              <span>info@icell.io</span>
+            </a>
+          </div>
         </div>
+
+
+        <div className="social-container">
+          <h1>Let&apos;s Connect</h1>
+
+          <div className="socials">
+            {footerLinks.map((links, idx) => (
+              <a key={idx} href={links.link} target="_blank" rel="noreferrer">
+                <links.icon />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <hr style={{ width: "auto" }} />
+
+      <div className="lower-section">
+        <h1>Made with ❣️ by <span>Tech Team</span></h1>
       </div>
     </footer>
   )
