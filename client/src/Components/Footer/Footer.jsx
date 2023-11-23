5f36bfd2a2e66dbed4b15f8cf73e686afd41b1ab
Rohit <rohitmondall8000@gmail.com>
2023-11-23 21:40:14 +0530

    footer section

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -36,100 +36,60 @@ export default function Footer() {
         <div className="footer-logo">
           <img src={ICellImg} alt="Icell Logo" />
           <h2>Innovation Cell</h2>
-          <footer>
-            <div className="footer-logo">
-              <img src={ICellImg} alt="Icell Logo" />
-              <h2>Innovation Cell</h2>
-            </div>
+        </div>
 
-            <div className="about-container container">
-              <h1>I-Cell</h1>
+        <div className="about-container container">
+          <h1>I-Cell</h1>
 
-              <div className="about">
-                <a className="footer-a" href="#aboutusnav">
-                  About Us
-                </a>
-                <a className="footer-a" href="#eventsnav">
-                  Events
-                </a>
-                <a className="footer-a" href="#spons">
-                  Sponsors
-                </a>
-              </div>
-
-<<<<<<< D
-
-  <div className="about-container container">
-    <h1>I-Cell</h1>
-
-    <div className="about">
-      <a href="#aboutusnav">About Us</a>
-      <a href="#eventsnav">Events</a>
-      <a href="#spons">Sponsors</a>
-      <a href="#team">Team</a>
-    </div>
-=======
-      <div className="container">
-      <h1>Contact</h1>
-
-      <div className="contacts">
-        <a
-          href="https://maps.app.goo.gl/EWSsuCfKTZvQZBxCA"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <BsPinMapFill style={{ margin: "1px 15px 0 0", scale: "150%" }} />
-          <span>NIT Raipur, Raipur, CG 492013</span>
-        </a>
-
-        <a href="mailto:info@icell.io" target="_blank" rel="noreferrer">
-          <BsEnvelope style={{ margin: "1px 15px 0 0", scale: "150%" }} />
-          <span>Email</span>
-        </a>
->>>>>>> 935899aab19a0d3ab4b56ace3eaf040188cd4222
-      </div>
-
-<<<<<<< HEAD
+          <div className="about">
+            <a href="#aboutusnav">About Us</a>
+            <a href="#eventsnav">Events</a>
+            <a href="#spons">Sponsors</a>
+            <a href="#team">Team</a>
+          </div>
+        </div>
 
         <div className="container">
           <h1>Contact</h1>
-=======
-      <div className="social-container">
-      <h1>Let&apos;s Connect</h1>
->>>>>>> 935899aab19a0d3ab4b56ace3eaf040188cd4222
 
-      <div className="contacts">
-        <a href="https://maps.app.goo.gl/EWSsuCfKTZvQZBxCA" target="_blank" rel="noreferrer">
-          <BsPinMapFill style={{ scale: "120%" }} />
-          <span>NIT Raipur, Raipur, CG 492013</span>
-        </a>
+          <div className="contacts">
+            <a
+              href="https://maps.app.goo.gl/EWSsuCfKTZvQZBxCA"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsPinMapFill style={{ margin: "0px 10px 0 0", scale: "120%" }} />
+              <span>NIT Raipur, Raipur, CG 492013</span>
+            </a>
 
-        <a href="mailto:info@icell.io" target="_blank" rel="noreferrer">
-          <BsEnvelope style={{ scale: "120%" }} />
-          <span>info@icell.io</span>
-        </a>
+            <a href="mailto:info@icell.io" target="_blank" rel="noreferrer">
+              <BsEnvelope style={{ margin: "0px 10px 0 0", scale: "120%" }} />
+              <span>Email</span>
+            </a>
+          </div>
+        </div>
+
+
+        <div className="social-container">
+          <h1>Let&apos;s Connect</h1>
+
+          <div className="socials">
+            {footerLinks.map((link) => (
+              <a href={link.link} key={link.link} target="_blank">
+                <link.icon />
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
 
 
-    <div className="social-container">
-      <h1>Let&apos;s Connect</h1>
+      <hr style={{ width: "auto" }} />
 
-      <div className="socials">
-        {footerLinks.map((links, idx) => (
-          <a key={idx} href={links.link} target="_blank" rel="noreferrer">
-            <links.icon />
-          </a>
-        ))}
+
+      <div className="lower-section">
+        <h1>Made with ❣️ by <span>Tech Team</span></h1>
       </div>
-    </div>
-  </div>
-
-    <hr style={{ width: "auto" }} />
-
-    <div className="lower-section">
-      <h1>Made with ❣️ by <span>Tech Team</span></h1>
-    </div>
-    </footer >
+    </footer>
   );
 }
