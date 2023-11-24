aa314a1a6d718a74e04d5a3a8384035b290d79c2
Prince <princetripathi093@gmail.com>
2023-11-24 12:56:02 +0530

    stash

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -9,12 +9,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import Sponsors from "./Components/Sponsors/Sponsors";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import MakerSpace from "./Components/MakerSpace/MakerSpace";
-import Team from "./Components/Team/Team";
 import SnowfallComponent from "./Components/Snowfall/SnowfallComponent";
-import NameCard from "./Components/Team/NameCard";
-import Executives from "./Components/Team/Executives";
 import Contact from "./Components/ContactUs/Contact";
-import Moon from "./Assets/Moon";
 import Accordian from "./Components/Faq/Accordian";
 import Timeline from "./Components/Timeline/Timeline";
 import Footer from "./Components/Footer/Footer";
@@ -71,6 +67,8 @@ function App() {
 
         <div id="eventsnav"></div>
         <Timeline />
+        <div id="faqnav"></div>
+        <Accordian />
         <div id="sponsnav"></div>
         <Sponsors />
         <div id="contactusnav"></div>
@@ -80,8 +78,6 @@ function App() {
          
         </div> */}
       {/* <MakerSpace /> */}
-      <div id="faqnav"></div>
-      <Accordian />
       <Footer />
     </>
   );
