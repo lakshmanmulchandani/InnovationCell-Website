814fd9c48741dddbe232521205274ad22f6f2a16
Pranjal Naman <pranjalofficial28@gmail.com>
2024-01-26 19:08:31 +0530

    stah

--- client/src/Components/cursor/Cursor.jsx
+++ client/src/Components/cursor/Cursor.jsx
@@ -1,62 +1,3 @@
-// // CustomCursor.jsx
-
-// import React, { useState, useEffect } from "react";
-// import "./Cursor.css"; // Import your CSS file for styling
-// import pointerYellow from "./pointer-yellow.png";
-// import cursorYellow from "./cursor-yellow.png";
-// import clickAnimation from "./onclick_animation.gif";
-
-// const Cursor = () => {
-//   const [position, setPosition] = useState({ x: 0, y: 0 });
-//   const [isHovered, setIsHovered] = useState(false);
-//   const [isClicked, setIsClicked] = useState(false);
-
-//   useEffect(() => {
-//     const updateCursorPosition = (e) => {
-//       setPosition({ x: e.clientX, y: e.clientY });
-//     };
-
-//     document.addEventListener("mousemove", updateCursorPosition);
-
-//     return () => {
-//       document.removeEventListener("mousemove", updateCursorPosition);
-//     };
-//   }, []);
-
-//   const handleCursorHover = () => {
-//     setIsHovered(true);
-//   };
-
-//   const handleCursorLeave = () => {
-//     setIsHovered(false);
-//   };
-
-//   const handleCursorClick = () => {
-//     setIsClicked(!isClicked);
-//   };
-
-//   return (
-//     <div
-//       className={`custom-cursor ${isHovered ? "hovered" : ""} ${
-//         isClicked ? "clicked" : ""
-//       }`}
-//       style={{
-//         left: `${position.x}px`,
-//         top: `${position.y}px`,
-//         transform: `translate(-50%, -50%) scale(${
-//           isHovered || isClicked ? 2 : 1
-//         })`,
-//       }}
-//       onMouseDown={handleCursorClick}
-//       onMouseUp={handleCursorClick}
-//     >
-//       <img src={clickAnimation} />
-//     </div>
-//   );
-// };
-
-// export default Cursor;
-
 import React, { useState, useEffect } from "react";
 import "./Cursor.css"; // Assuming you have a CSS file for styling
 import quill_cursor from "./quill_cursor.png";
@@ -95,6 +36,7 @@ const Cursor = () => {
       style={{ left: `${position.x}px`, top: `${position.y}px` }}
     >
       <img src={quill_cursor} height={70} alt="custom-cursor" />
+      
     </div>
   );
 };
